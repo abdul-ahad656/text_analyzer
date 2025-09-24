2bfe7313a4a44a606b2b59690cfda4662408a973
Abdul Ahad <adibutt457@gmail.com>
2025-09-24 14:46:09 +0500

    Fix start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 set -e
 
+# Install python & pip
+apt-get update && apt-get install -y python3 python3-pip
+
 cd backend
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 gunicorn app:app --bind 0.0.0.0:$PORT
